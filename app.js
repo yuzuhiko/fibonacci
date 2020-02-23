b98d8f111d0ecfd562d7d33b7e5c934294ab7ba4
yuzuhiko <deedee1668-create@yahoo.co.jp>
2020-02-23 22:27:42 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 'use strict';
+
+const memo = new Map();
+
+memo.set(0,0);
+memo.set(1,1);
+
 function fib(n){
+    /*
     if (n === 0){
         return 0;
     } else if (n === 1) {
@@ -8,6 +15,14 @@ function fib(n){
     
     //return (n-2) + (n - 1); //これだじゃめか。。。
     return fib(n-1) + fib(n-2);
+    */
+    if (memo.has(n)){
+        return memo.get(n);
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
+
 }
 
 const length = 40;
